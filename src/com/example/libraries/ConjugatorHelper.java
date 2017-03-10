7b052304712165d7a2a80db53f952e126b3a3056
Rahul Ramkumar <rahulram226@gmail.com>
2017-03-09 20:03:51 -0500

    Add Shoe Verbs and More Methods to the Helper Class

--- src/com/example/libraries/ConjugatorHelper.java
+++ src/com/example/libraries/ConjugatorHelper.java
@@ -5,6 +5,7 @@ package com.example.libraries;
  */
 public class ConjugatorHelper {
 	public enum VerbType {AFFIRMATIVE, NEGATIVE}
+	public enum ShoeVerbType {E_TO_IE, O_TO_UE, E_TO_I, O_TO_HUE, U_TO_UE}
 
 	public static String replaceLastOccurrence(String string, String substring, String replacement) {
 		int index = string.lastIndexOf(substring);
@@ -14,5 +15,25 @@ public class ConjugatorHelper {
 
 		return string.substring(0, index) + replacement
 				+ string.substring(index + substring.length());
+	}
+
+	public static String replaceLastOccurrenceExceptEnding(String string, String substring, String replacement) {
+		// Save ending for later when necessary to be added back
+		String ending = getLastNumberOfLetters(string, 2);
+
+		// Take out the ending
+		string = replaceLastOccurrence(string, getLastNumberOfLetters(string, 2), "");
+
+		string = replaceLastOccurrence(string, substring, replacement);
+
+		return string + ending; // Put everything together
+	}
+
+	public static String getLastNumberOfLetters(String string, int characters) {
+		return string.substring(string.length() - characters);
+	}
+
+	public static boolean isVowel(char c) {
+		return "aeiou".indexOf(c) != -1;
 	}
 }
