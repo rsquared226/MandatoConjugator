b633ed650ce804e67dcb9d86264d3c23990c1064
Rahul Ramkumar <rahulram226@gmail.com>
2017-03-10 00:59:34 -0500

    Create MainActivity Layout

--- app/src/main/java/com/rahulramkumar/mandatoconjugator/MainActivity.java
+++ app/src/main/java/com/rahulramkumar/mandatoconjugator/MainActivity.java
@@ -1,13 +1,38 @@
 package com.rahulramkumar.mandatoconjugator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	Spinner subjectSpinner;
+	Switch verbTypeSwitch;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		subjectSpinner = (Spinner) findViewById(R.id.spinner_subject);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+				R.array.subjects_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		subjectSpinner.setAdapter(adapter);
+
+		verbTypeSwitch = (Switch) findViewById(R.id.switch_type);
+		verbTypeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+				if (b) {
+					verbTypeSwitch.setText(R.string.affirmative);
+				} else {
+					verbTypeSwitch.setText(R.string.negative);
+				}
+			}
+		});
+	}
 }
